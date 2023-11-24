154365717afed190fbfee7924f3a30ab22aadb46
Atle Aarrestad <atle.aarrestad@ad.eyeshare.no>
2023-11-24 21:28:39 +0100

    animation test

---
+++ AnimatedSprite2D.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public partial class AnimatedSprite2D : Godot.AnimatedSprite2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+    {
+		this.SpeedScale = 2.0f;
+		this.Play("default");
+
+    }
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+
+	}
+}
