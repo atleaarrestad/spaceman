e44f696d4ebffb40a0347d9c11f2b11c8e776cfa
Atle Aarrestad <atle.aarrestad@ad.eyeshare.no>
2023-11-25 21:43:27 +0100

    gætter on airplane

--- components/EquipmentContainer.cs
+++ components/EquipmentContainer.cs
@@ -4,17 +4,25 @@ using System.Collections.Generic;
 using System.Linq;
 
 [GlobalClass]
-public partial class EquipmentContainer : Node
+public partial class EquipmentContainer : Node2D
 {
 	[Export]
 	public PackedScene WeaponScene { get; set; }
-	private Node WeaponNode;
+	[Export]
+	public Vector2 Offset { get; set; } = Vector2.Zero;
+	[Export]
+	public Node2D ParentNode { get; set; }
+
+	private Node2D WeaponNode;
 	private EquipmentComponent equipmentComponent;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		WeaponNode = WeaponScene.Instantiate();
+		WeaponNode = WeaponScene.Instantiate<Node2D>();
 		AddChild(WeaponNode);
+		WeaponNode.Position += Offset;
+
 		equipmentComponent = WeaponNode.GetNode<EquipmentComponent>("EquipmentComponent");
 		equipmentComponent.Fire();
 
