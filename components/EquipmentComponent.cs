c83bea9b896948fe7c056d3a800e335110551e38
Atle Aarrestad <atle.aarrestad@ad.eyeshare.no>
2023-11-26 01:16:47 +0100

    cooldown boolet

--- components/EquipmentComponent.cs
+++ components/EquipmentComponent.cs
@@ -6,12 +6,18 @@ public partial class EquipmentComponent : Node2D
 {
 	[Export]
 	public EquipmentStats Stats { get; set; }
-	// Called when the node enters the scene tree for the first time.
+	private Timer timer;
+	private bool canShoot = true;
+
 	public override void _Ready()
 	{
+		timer = new Timer();
+		timer.OneShot = true;
+		AddChild(timer);
+
+		timer.Timeout += () => canShoot = true;
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 
@@ -19,9 +25,13 @@ public partial class EquipmentComponent : Node2D
 
 	public void Activate()
 	{
-		GD.Print($"Gun with name: {Stats.Name} was activated");
-		var boolet = Stats.Projectile.Instantiate<Area2D>();
-		AddChild(boolet);
-
-	}
+		if (canShoot)
+		{
+			var projectile = Stats.Projectile.Instantiate<Area2D>();
+			GetTree().Root.AddChild(projectile);
+			projectile.Position = new Vector2(GlobalPosition.X, GlobalPosition.Y);
+			canShoot = false;
+			 timer.Start(Stats.Cooldown);
+		}
+	} 
 }
